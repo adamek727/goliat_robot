6b2c46c7f4ac9a5d57036e9f1199c76c18d9412d
Adam Ligocki <xligoc01@stud.feec.vutbr.cz>
2020-02-10 04:55:00 -1000

    Initial commit

---
+++ include/udp_listener.h
@@ -0,0 +1,80 @@
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define PORT     8080
+#define MAXLINE 1024
+#define MAX_SPEED_MSG_TTL 50
+
+enum UdpStatus{
+    Ok,
+    Failed,
+};
+
+static int left_wheel_speed_target = 0;
+static int right_wheel_speed_target = 0;
+
+unsigned int speed_msg_ttl = MAX_SPEED_MSG_TTL;
+static enum UdpStatus udp_status = Ok;
+
+void* udpListener()
+{
+    int sockfd;
+    char buffer[MAXLINE];
+    struct sockaddr_in servaddr, cliaddr;
+
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("Unable to create socket");
+        udp_status = Failed;
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    servaddr.sin_family    = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 ) {
+        perror("Unable to bind port");
+        udp_status = Failed;
+        exit(EXIT_FAILURE);
+    }
+
+    int n;
+    socklen_t len;
+    fd_set readfds;
+
+    fcntl(sockfd, F_SETFL, O_NONBLOCK);
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 10000;
+    while(true) {
+        select(sockfd+1, &readfds, NULL, NULL, &tv);
+        n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+                    MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+                    &len);
+        if(n > 0) {
+            printf("udp received: %s", buffer);
+            char **arr = NULL;
+            unsigned int c = split(buffer, ',', &arr);
+            if(strcmp("$speed", arr[0]) == 0) {
+                left_wheel_speed_target = atoi(arr[1]);
+                right_wheel_speed_target = atoi(arr[2]);
+                speed_msg_ttl = MAX_SPEED_MSG_TTL;
+            }
+        } else {
+
+        }
+        usleep(10000);
+    }
+}
---
+++ src/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "i2c.h"
+#include "km2.h"
+#include "split.h"
+#include "udp_listener.h"
+
+#define RAMP_CONSTANT 0.2
+//#define DEBUG
+
+void debug_printout();
+
+static int left_wheel_speed = 0;
+static int right_wheel_speed = 0;
+
+int main(int argc, char* argv[]) {
+		
+    int fd = i2c_init(1);
+
+    pthread_t udpListener_tid;
+    pthread_create(&udpListener_tid, NULL, udpListener, NULL);
+
+    while(1) {
+
+        if(speed_msg_ttl > 0) {
+            left_wheel_speed += (left_wheel_speed_target-left_wheel_speed) * RAMP_CONSTANT;
+            right_wheel_speed += (right_wheel_speed_target-right_wheel_speed) * RAMP_CONSTANT;
+            speed_msg_ttl--;
+        } else {
+            left_wheel_speed_target = right_wheel_speed_target = 0;
+            left_wheel_speed = right_wheel_speed = 0;
+        }
+
+        km2_drive(fd, 0x71, (int16_t)left_wheel_speed, (int16_t)right_wheel_speed);
+#ifdef DEBUG
+        debug_printout();
+#endif
+        if(udp_status == Failed) {
+            printf("UDP Failed ... exiting!");
+            break;
+        }
+        usleep(100000); // 100 ms
+    }
+
+    i2c_close(fd);
+    return 0;
+}
+
+
+
+void debug_printout() {
+    printf(" * * * * * * * * * *\n");
+    printf("targets-> l: %d r: %d\n", left_wheel_speed_target, right_wheel_speed_target);
+    printf("real-> l: %d r: %d\n", left_wheel_speed, right_wheel_speed);
+    printf("speed msg ttl: %d\n", speed_msg_ttl);
+}
+
+
